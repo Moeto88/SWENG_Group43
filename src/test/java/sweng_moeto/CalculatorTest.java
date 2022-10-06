4994b44817af62a781ecd91310922dd55027a0c0
Moeto88 <sonodam@tcd.ie>
2022-10-06 23:15:06 +0100

    test cases

---
+++ src/test/java/sweng_moeto/CalculatorTest.java
@@ -0,0 +1,27 @@
+package sweng_moeto;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testInfixToPostFix() {
+		String equation = "14*3-7*35+22-56+100";
+		String[] expectedResult = {"14", "3", "*", "7", "35", "*", "-", "22", "+", "56", "-", "100", "+"};
+		assertEquals(Arrays.toString(expectedResult), Calculator.infixToPostFix(equation).toString());
+	}
+	
+	@Test
+	public void testEvaluatePostfix() {
+		String equation = "14*3-7*35+22-56+100";
+		int expectedResult = -137; 
+		assertEquals(expectedResult, Calculator.evaluatePostfix(Calculator.infixToPostFix(equation)));
+	}
+	
+
+}
